13d12ada58c92f69cb309f784cf52000e4e77bc1
unknown <lehuynhhoaivan2001@gmail.com>
2025-11-14 22:09:17 +0700

    Naming Routes

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,29 @@ Route::get('/', function () {
 
 Route::get('/about', function () {
     return '<h1>About Page</h1>';
-});
+})->name('about.page');
 
 Route::get('/contact', function () {
     return '<h1>Contact Page</h1>';
 });
 
-
 Route::get('/news/{id}', function ($id) {
     return "<h1>New = {$id}</h1>";
-});
+})->name('news.page');
 
 Route::get('/product/{id}', function ($id) {
     return '<h1>Product = '.$id.'</h1>';
 });
+
+Route::get('/home', function () {
+    return "<h2><a href='/about'>About</a></h2>";
+});
+
+Route::get('/content', function () {
+    return "<h2><a href='".route('about.page')."'>About</a></h2>";
+});
+
+Route::get('/course', function () {
+    return "<h2><a href='".route('news.page', ['id' => 2])."'>News</a></h2>";
+});
+
