f811316e57b51d8242e9347b18144a81eb35eeaa
unknown <lehuynhhoaivan2001@gmail.com>
2026-01-16 09:13:21 +0700

    Update Data In Database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,31 +11,45 @@ class ProductController extends Controller
 {
     function index() {
 
-    DB::table('products')->insert([
-        [   'name' => 'Sản phẩm 1',
-            'slug' => 'san-pham-1',
-            'description' => 'Mô tả sản phẩm 1',
-            'price' => 150000,
-            'quantity' => 20,
-            'image' => 'product-new1.jpg',
-            'status' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],[
-            'name' => 'Sản phẩm 2',
-            'slug' => 'san-pham-2',
-            'description' => 'Mô tả sản phẩm 2',
-            'price' => 200000,
-            'quantity' => 15,
-            'image' => 'product-new2.jpg',
-            'status' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]    
-    ]);
-        dd('Đã thêm sản phẩm mới');
-        $products = Product::all();
+    // DB::table('products')->insert([
+    //     [   'name' => 'Sản phẩm 1',
+    //         'slug' => 'san-pham-1',
+    //         'description' => 'Mô tả sản phẩm 1',
+    //         'price' => 150000,
+    //         'quantity' => 20,
+    //         'image' => 'product-new1.jpg',
+    //         'status' => 1,
+    //         'created_at' => now(),
+    //         'updated_at' => now(),
+    //     ],[
+    //         'name' => 'Sản phẩm 2',
+    //         'slug' => 'san-pham-2',
+    //         'description' => 'Mô tả sản phẩm 2',
+    //         'price' => 200000,
+    //         'quantity' => 15,
+    //         'image' => 'product-new2.jpg',
+    //         'status' => 1,
+    //         'created_at' => now(),
+    //         'updated_at' => now(),
+    //     ]    
+    // ]);
+    //     dd('Đã thêm sản phẩm mới');
+    //     $products = Product::all();
 
+        // return DB::table('products')->where('id', 53)->get();
+        DB::table('products')->where('id', 53)->update([
+            'name' => 'Sản phẩm cập nhật 53',
+            'slug' => 'san-pham-cap-nhat-53',
+            'description' => 'Mô tả sản phẩm cập nhật 53',
+            'price' => 175000,
+            'quantity' => 30,
+            'image' => 'product-update53.jpg',
+            'status' => 1,
+            'updated_at' => now(),
+        ]);
+        dd('Đã cập nhật sản phẩm có ID = 53');
+        $products = Product::all();
+        
         // Query Builder: Lấy tất cả sản phẩm từ bảng products
         // return DB::table('products')->get();
 
